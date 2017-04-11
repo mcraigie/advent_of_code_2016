361dc11fb3b101b2f564c8562fd3eb1c2f1e5e17
mcraigie <max.craigie@gmail.com>
2017-04-11 12:51:08 +1000

    revise bonus solution

--- day_3/bonus.rb
+++ day_3/bonus.rb
@@ -5,23 +5,15 @@ def valid_triangle?(side_lengths)
 	true unless triangle_invalid
 end
 
-def clean(line)
-	line.gsub(/\s+/, ' ').strip.split(' ').map(&:to_i)
-end
+input = File.read(ARGV[0]).split("\n")
+rows = input.map{ |row| row.split.map(&:to_i)}
+rows = rows.transpose
 
 triangle_count = 0
 
-lines = File.read(ARGV[0]).split("\n")
-
-lines.each_slice(3) do |batch|
-	sides_a = clean(batch[0])
-	sides_b = clean(batch[1])
-	sides_c = clean(batch[2])
-
-	i = 0
-	loop do
-		triangle_count += 1 if valid_triangle?([sides_a[i], sides_b[i], sides_c[i]])
-		break if (i += 1) == 3
+rows.each do |row|
+	row.each_slice(3) do |triangle|
+		triangle_count += 1 if valid_triangle?(triangle)
 	end
 end
 
