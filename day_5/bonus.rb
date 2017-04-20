32a8ea1523b84f28fded15b07ce7c88dacd6a862
mcraigie <max.craigie@gmail.com>
2017-04-20 18:33:00 +1000

    initialise array better

--- day_5/bonus.rb
+++ day_5/bonus.rb
@@ -2,16 +2,17 @@ require 'digest/md5'
 
 door_id = ARGV[0]
 i = 0
-password = []
+password = [nil] * 8
 
 loop do
 	h = Digest::MD5.hexdigest("#{door_id}#{i}")
 	if h[0,5] == ('00000') and h[5].to_i.to_s == h[5] and password[h[5].to_i] == nil 
 		password[h[5].to_i] = h[6]
 		password = password.first(8)
+		p password
 	end
 
-	break if !password.any?{ |e| e.nil? } and password.length == 8
+	break if !password.any?{ |e| e.nil? }
 	i += 1
 end
 
